47996ad2a4e49fae8554d6d5ad07548212348939
tahirikosan <tahirabuzetoglu@gmail.com>
2019-03-09 12:55:39 +0300

    SaldiriKodTamam

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private float movementSpeed; //Hareket hızı için değişken yaptık
+
+    private bool isAttack;
+
+    private Rigidbody2D myRigidBody2D; //Kodun bağlı olduğu objenin RigidBody'sine değişken yaptık
+    //Yeni
+    private Animator myAnimator; //Kodun bağlı olduğu objenin Animator'una değişken yaptık
+
+
+    private bool facingRight; //Karakterin sağa bakıp bakmadığına göre true yada false alır.
+
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>(); //myRigidBody2D değişkenini Editörde bulunan Rigidbody'ye referans yaptık.
+        //Yeni
+        myAnimator = GetComponent<Animator>(); //myAnimator değişkenini Editörde bulunan Animator'e referans yaptık.
+        facingRight = true;  //Oyun başladığında karakter sağa baktığı için true değeri veriyoruz.
+    }
+
+
+    private void Update()
+    {
+        HandleInputs();
+    }
+
+    void FixedUpdate()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");  //Horizontal sistemi için değişken yaptık.horizontal değişkeni a,d ve yön tuşlarına göre -1 vey 1 değeri alacak.
+
+        HandleMovement(horizontal);
+        Flip(horizontal);
+        HandleAttack();
+        ResetValues();
+    }
+
+
+    private void HandleMovement(float horizontal)
+    {
+        if (!this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack")) {
+            myRigidBody2D.velocity = new Vector2(horizontal * movementSpeed, myRigidBody2D.velocity.y);
+        }
+
+        
+        myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+    }
+
+    private void HandleAttack() {  //Saldırı Kontrol
+        if (isAttack && !this.myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack")) {
+            myAnimator.SetTrigger("attack");
+            myRigidBody2D.velocity = Vector2.zero;
+        }
+    }
+
+    private void HandleInputs() {  //Kalvye Girişleri
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            isAttack = true;
+        }
+    }
+
+    private void ResetValues() {
+        isAttack = false;
+    }
+
+    private void Flip(float horizontal)
+    { //Horizontal'a bağlı olarak karakterin yönünü değiştirir. 
+
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale; //Karakter scale'ine referans yaptık.
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+
+}
